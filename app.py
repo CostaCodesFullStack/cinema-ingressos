c35db7e66c792891699f37168f6842efe26ba3f5
Jpepo <jpepo@example.com>
2025-10-26 00:22:32 -0300

    Corrige nome da variavel

--- app.py
+++ app.py
@@ -94,7 +94,8 @@ def index():
         filtrados = {nome: dados for nome, dados in filmes.items() if termo in nome.lower()}
     else:
         filtrados = filmes
-    vendas_por_filmes = contar_vendas_por_filme(historico)
+    vendas_por_filme = contar_vendas_por_filme(historico)
+    
     return render_template("index.html", filmes=filtrados, vendas_por_filme=vendas_por_filme)
 
 
